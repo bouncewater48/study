d6da609d0a6cf5a5da1ab0a1927de3ed9aa3c3c9
bouncewater48 <bouncewater48@gmail.com>
2025-07-31 17:03:50 +0900

    js day5 추가

---
+++ js day05/js/async-await.js
@@ -0,0 +1,13 @@
+// 6번 : async / await
+
+// async : 내부에서 return한 값을 자동으로 Promise.resolve(value)로 변환하는 함수
+async function getName(){
+  return "짱구"; // 자동으로 Promise로 감싸짐
+}
+
+async function printName(){
+  const name = await getName(); //await getName()은 getName()함수의 Promise가 이행(resolve) 될때까지 기다림
+  console.log(name);
+}
+
+printName();
